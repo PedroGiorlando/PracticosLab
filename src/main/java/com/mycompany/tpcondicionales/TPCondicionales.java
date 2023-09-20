a0897599b3efdb34d7d4fd57cdba0427bcc229ad
PedroGiorlando <pggv2003@gmail.com>
2023-09-20 01:02:47 -0300

    Prueba

---
+++ src/main/java/com/mycompany/tpcondicionales/TPCondicionales.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.tpcondicionales;
+
+
+public class TPCondicionales {
+
+    public static void main(String[] args) {
+        System.out.println("Aca voy a hacer el tp de condicionales");
+    }
+}
