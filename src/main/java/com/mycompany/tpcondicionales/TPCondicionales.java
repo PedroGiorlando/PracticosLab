8c527bb1ba882e495f7ad4de6ae1a320e2fb5eb0
PedroGiorlando <pggv2003@gmail.com>
2023-09-20 01:11:16 -0300

    Agregue texto

--- src/main/java/com/mycompany/tpcondicionales/TPCondicionales.java
+++ src/main/java/com/mycompany/tpcondicionales/TPCondicionales.java
@@ -6,5 +6,6 @@ public class TPCondicionales {
 
     public static void main(String[] args) {
         System.out.println("Aca voy a hacer el tp de condicionales");
+        System.out.println("Gonza sos un boludo");
     }
 }
