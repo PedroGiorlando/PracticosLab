757fb6702d6a6854a0c5cb58bbc6b5e72319a336
PedroGiorlando <pggv2003@gmail.com>
2023-09-24 12:53:31 -0300

    TP Generales

---
+++ TPGenerales/src/main/java/com/mycompany/tpgenerales/TPGenerales.java
@@ -0,0 +1,23 @@
+package com.mycompany.tpgenerales;
+import java.util.Scanner;
+
+public class TPGenerales {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        /*Ejercicio1
+        int num1, num2, aux;
+        System.out.println("Ingrese el valor de la primer variable: ");
+        num1 = sc.nextInt();
+        System.out.println("Ingrese el valor de la segunda variable");
+        num2 = sc.nextInt();
+        
+        System.out.println("Ahora la variable 1 tendrá el valor de la variable 2 y viceversa");
+        aux = num1;
+        num1 = num2;
+        num2 = aux;
+        System.out.println("Variable 1: "+ num1 + ", Variable 2: "+ num2);*/
+        
+        
+    }
+}
