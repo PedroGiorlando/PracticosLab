0b889b0667fbac28c98cf68d69d0ee9205e221af
PedroGiorlando <pggv2003@gmail.com>
2023-10-04 17:22:18 -0300

    TP Funciones

---
+++ TPFunciones/src/main/java/com/mycompany/tpfunciones/TPFunciones.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tpfunciones;
+
+/**
+ *
+ * @author Usuario
+ */
+public class TPFunciones {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
